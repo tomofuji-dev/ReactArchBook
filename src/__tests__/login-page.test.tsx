def5c12bf5ae620b46222bb617f0d9168e70c4ef
tfuji <tomofuji.dev@gmail.com>
2023-07-24 16:49:57 +0900

    add login test

---
+++ src/__tests__/login-page.test.tsx
@@ -0,0 +1,58 @@
+import { act } from 'react-dom/test-utils';
+
+import LoginPage from '@/pages/auth/login';
+import {
+  appRender,
+  screen,
+  userEvent,
+  waitFor,
+} from '@/testing/test-utils';
+
+const router = {
+  replace: jest.fn(),
+  query: {},
+};
+
+jest.mock('next/router', () => ({
+  useRouter: () => router,
+}));
+
+describe('Login Page', () => {
+  it('should login the user into the dashboard', async () => {
+    await appRender(<LoginPage />);
+
+    const emailInput = screen.getByRole('textbox', {
+      name: /email/i,
+    });
+
+    const passwordInput =
+      screen.getByLabelText(/password/i);
+
+    const submitButton = screen.getByRole('button', {
+      name: /log in/i,
+    });
+
+    const credentials = {
+      email: 'user1@test.com',
+      password: 'password',
+    };
+
+    act(() => {
+      userEvent.type(emailInput, credentials.email);
+    });
+
+    act(() => {
+      userEvent.type(passwordInput, credentials.password);
+    });
+
+    act(() => {
+      userEvent.click(submitButton);
+    });
+
+    await waitFor(() =>
+      expect(router.replace).toHaveBeenCalledWith(
+        '/dashboard/jobs'
+      )
+    );
+  });
+});
